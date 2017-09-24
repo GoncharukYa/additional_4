34c7dc87bc80fa95fc1097188d30d6c595f4f7af
Goncharuk Yaroslav <gonyarik@gmail.com>
2017-09-24 21:46:14 +0300

    First

--- src/index.js
+++ src/index.js
@@ -1,3 +1,21 @@
 module.exports = function multiply(first, second) {
-  // your solution
+  if (first.length > second.length) {
+    let arrFirst = first.split(' ');
+    //let arrSecond = second.split(' ');
+  } else {
+    let arrFirst = second.split('');
+    //let arrSecond = first.split('');
+  }
+  let result = [];
+  let memory = 0;
+  for (let i = arrSecond.length - 1; i >= 0; i--) {
+    let product = arrFirst * arrSecond + memory;
+    result.push(product % 10);
+    memory = product / 10;
+  }
+
+
+
+
+  return String(product.reverse());
 }
